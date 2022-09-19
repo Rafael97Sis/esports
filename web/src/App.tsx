d90c7092be40f33de9d16c6ed843533160be8514
Rafael Rodrigues <33671644+Rafael97Sis@users.noreply.github.com>
2022-09-19 00:31:50 -0300

    update - 0.4
    # - Atualização conexão -API
    # - Tela Modal - Cadastro

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
 import './styles/main.css'
+import * as Dialog from '@radix-ui/react-dialog'
 import { useState, useEffect } from 'react'
 import { GameBanner } from './components/GameBanner'
 import { CreateAdBanner } from './components/CreateAdBanner'
 import logoImg from './assets/Logo-nlw-esports.svg'
+import { GameController } from 'phosphor-react'
 
 function App() {
   interface Game {
@@ -24,32 +26,89 @@ function App() {
   }, [])
 
   return (
-
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20' >
       <img src={logoImg} alt="logo-nlw" />
 
       <h1 className='text-5xl text-white font-black mt-20 ' >
         Seu <span className="bg-color-nlw-gradie bg-clip-text text-transparent "> duo </span> está aqui.
       </h1>
-
-
-
       <div className='grid grid-cols-6 gap-6 mt-16 ' >
         {games.map(game => {
           return (
             <GameBanner
+              key={game.id}
               title={game.title}
-              bannerUrl={game.bannerUrl}              
+              bannerUrl={game.bannerUrl}
               adsCounter={game._count.ads}
             />
           )
         })}
       </div>
+      <Dialog.Root>
+        <CreateAdBanner />
 
-      <CreateAdBanner />
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
+          <Dialog.Content className="fixed bg-[#2a2634] py-8 px-10 text-white top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 rounded-lg w-[400px] shadow-lg shadow-white/25 ">
+
+            <Dialog.Title className="text-3x1 text-white font-black"> Publique um anúncio </Dialog.Title>
+
+            <Dialog.Content>
+              <form className=''>
+                <div>
+                  <label htmlFor='game'> Qual o game</label>
+                  <input id="game" placeholder='selecione o game que deseja jogar' ></input>
+                </div>
+
+                <div>
+                  <label htmlFor='game'> Seu nome (ou nickname) </label>
+                  <input id="game" placeholder='Como te chamam Dentro do game ?' ></input>
+                </div>
+
+                <div>
+                  <div >
+                    <label htmlFor="yearsPlaying" > Joga a quantos Anos ? </label>
+                    <input id="yearPlaying" type="number" placeholder='Tudo bem ser ZERO'  ></input>
+                  </div>
+                  <div>
+                    <label htmlFor='Discord' > Qual seu Discord ? </label>
+                    <input id="discord" type="text" placeholder="Usuario#2530" ></input>
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <label htmlFor='weekDays'> Quando Costuma jogar ?</label>
+                  </div>
+                  <div>
+                    <label htmlFor="hourStart"> Qual Horário do dia ?</label>
+                    <div>
+                      <input id="hourStart" type="time" placeholder="De"></input>
+                      <input id="hourEnd" type="time" placeholder='Até' ></input>
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <input type="checkbox" >
+                    Costumo me Conectar ao chat de voz
+                  </input>
+                </div>
+
+                <footer>
+                  <button> Cancelar </button>
+                  <button type="submit">
+                    <GameController />
+                    Encontrar duo
+                  </button>
+                </footer>
+              </form>
+            </Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
-
   )
 }
 
--- web/src/components/CreateAdBanner/index.tsx
+++ web/src/components/CreateAdBanner/index.tsx
@@ -1,17 +1,18 @@
 import { MagnifyingGlassPlus } from 'phosphor-react'
-
+import * as Dialog from '@radix-ui/react-dialog'
 export function CreateAdBanner() {
   return (
     <div className="pt-1 bg-color-nlw-gradie self-stretch  rounded-lg  mt-8 overflow-hidden ">
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-          <div>
-            <strong className='text-white font-black block text-2xl'> Não encontrou seu duo ? </strong>
-            <span className='text-zinc-400 block'>Publique um anúncio para Encontrar Novos players! </span>
-          </div>
-          <button className='py-3 px-4 rounded bg-violet-500 hover:bg-violet-700 text-white flex items-stretch  gap-2'>
-            <MagnifyingGlassPlus size={24} /> Publicar anúncio
-          </button>
+      <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+        <div>
+          <strong className='text-white font-black block text-2xl'> Não encontrou seu duo ? </strong>
+          <span className='text-zinc-400 block'>Publique um anúncio para Encontrar Novos players! </span>
         </div>
+        <Dialog.Trigger className='py-3 px-4 rounded bg-violet-500 hover:bg-violet-600 text-white flex items-center  gap-2'>
+          <MagnifyingGlassPlus size={24} />
+          Publicar anúncio
+        </Dialog.Trigger>
       </div>
+    </div>
   );
 }
